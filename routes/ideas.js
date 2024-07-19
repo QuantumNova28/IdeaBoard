186bd8955fb769af3152fc01651eebfd1a34d445
QuantumNova28 <suyogmathwale.escr@gmail.com>
2024-07-20 01:42:13 +0530

    CRUD Functionalities Added to the API

--- routes/ideas.js
+++ routes/ideas.js
@@ -25,9 +25,9 @@ const ideas = [
   },
 ];
 
-// Get all ideas
+// GET Requests
 router.get('/', (req, res) => {
-  res.json({ message: 'Welcome to IdeaBoard API' });
+  res.json({ message: 'Welcome to IdeaBoard API', data : ideas });
 });
 
 // Individual ids
@@ -39,6 +39,46 @@ router.get('/:id', (req, res) => {
       .json({ success: false, error: 'Resource not found' });
   }
   res.json({ success: true, data: idea });
+});
+
+// PUT Request
+
+router.put('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+
+// POST Request
+
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toLocaleDateString().slice(0, 10),
+  };
+
+  ideas.push = idea;
+  res.json({ success: true, data: idea });
+});
+
+// DELETE Request
+
+router.delete('/:id', (req, res) => {
+  const idea = ideas.findIndex((idea) => idea.id === +req.params.id);
+  if (idea !== -1) {
+    ideas.splice(idea, 1);
+  }
+  res.json({ success: 'True', data : ideas });
 });
 
 module.exports = router;
